85aa76c6bbe1de104464f96d6f3b659ed0efb5e3
Sambhaji12345 <sambhajikumbhar1977@gmail.com>
2025-02-11 14:20:11 +0530

    Delete cofee directory

--- cofee/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Coffee
-
-# Create your views here.
-def home(request):
-   cofee=Coffee.objects.all()
-   return render(request, 'home.html', {'cofee':cofee})
